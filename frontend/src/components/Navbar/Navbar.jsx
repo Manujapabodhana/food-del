76c7bf96aa7f205e07e4e838b75b2cead170d192
Manujapabodhana <rpmanuja123@gmail.com>
2025-08-17 14:18:06 +0530

    new

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      {/* Navbar content */}
+      <h1>My App</h1>
+    </div>
+  );
+}
+
+
+export default Navbar;
+
