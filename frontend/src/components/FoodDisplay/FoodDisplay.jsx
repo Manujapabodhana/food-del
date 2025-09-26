95fdd85461f9ea3b58b03fe9321a095ee4abcb35
Manujapabodhana <rpmanuja123@gmail.com>
2025-09-26 17:20:51 +0530

    ss

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../foodItem/foodItem'
 
 const FoodDisplay = ({category}) => {
 
@@ -12,17 +13,14 @@ const FoodDisplay = ({category}) => {
             {food_list.map((item, index) => {
                 if (category === "All" || category === item.category) {
                     return (
-                        <div key={index} className='food-item'>
-                            <img src={item.image} alt={item.name} />
-                            <div className="food-item-info">
-                                <div className="food-item-name-rating">
-                                    <p>{item.name}</p>
-                                    <img src="/src/assets/rating_starts.png" alt="rating" />
-                                </div>
-                                <p className="food-item-desc">{item.description}</p>
-                                <p className="food-item-price">${item.price}</p>
-                            </div>
-                        </div>
+                        <FoodItem 
+                            key={index}
+                            id={item._id}
+                            name={item.name}
+                            description={item.description}
+                            price={item.price}
+                            image={item.image}
+                        />
                     )
                 }
             })}
