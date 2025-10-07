222bc7289bee25f81d9540b2eacd8bc37e3ed389
Manujapabodhana <rpmanuja123@gmail.com>
2025-10-07 13:33:33 +0530

    x

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import
+import './AppDownload.css'
+import { assets } from '../../assets/assets'
 
 function AppDownload() {
   return (
-    <div>
-      
+    <div className='app-download' id='app-download'>
+        <p>For better experience download <br />Tomato App</p>
+        <div className="app-download-platforms">
+            <img src={assets.play_store} alt="Download from Play Store" />
+            <img src={assets.app_store} alt="Download from App Store" />
+        </div>
     </div>
   )
 }
